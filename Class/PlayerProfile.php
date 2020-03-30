fedfee3e056566adec2171b49b19360ead90d7cb
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-03-30 00:56:37 -0600

    Created new class to register athletes

--- Class/PlayerProfile.php
+++ Class/PlayerProfile.php
@@ -15,13 +15,28 @@ class PlayerProfile {
 	public function __construct($newAthleteId, $newFirstname, $newLastname, $newHeight, $newWeight, $newWingspan,
 										 $newHighschool,$newCollege,$newSport ) {
 try{
-$this->athleteId=$newAthleteId;
-$this->firstname = $newFirstname;
+$this->setAthleteId($newAthleteId);
+$this->setFirstname($newFirstname);
+$this->setLastname($newLastname);
+$this->setHeight($newHeight);
+$this->setWeight($newWeight);
+$this->setWingspan($newWingspan);
+$this->setHighschool($newHighschool);
+$this->setCollege($newCollege);
+$this->setSport($newSport);
 
 } catch(PDOException $exception){
 	$exception->getMessage();
 
 }
+
 }
+
+//Accessor for athlete firstname
+public function getFirstname(): string{
+		return $this->firstname;
+}
+
+
 
 }
